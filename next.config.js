d3767be5c12abb1d05c71a8d6851179d9ffb02bd
rafi arganti <43950887+rafia22110@users.noreply.github.com>
2026-01-17 09:29:00 +0200

    feat: Add Next.js config with i18n and image domains

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+  i18n: {
+    locales: ['he', 'en'],
+    defaultLocale: 'he',
+  },
+};
+
+module.exports = nextConfig;
